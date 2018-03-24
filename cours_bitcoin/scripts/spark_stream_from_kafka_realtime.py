d4c0b6caa23f9463c8cdfb68a8d30eecd5927b61
Sylrael <stephane.tiv@gmail.com>
2018-03-24 16:26:22 +0100

    merge historical and real-time data, and test pytest

--- cours_bitcoin/scripts/spark_stream_from_kafka_realtime.py
+++ cours_bitcoin/scripts/spark_stream_from_kafka_realtime.py
@@ -17,20 +17,28 @@ spark = SparkSession \
 sc = spark.sparkContext
 
 #Default values
+broker = 'localhost:9092'
+topic = 'bitcoin-realtime'
 host_es = 'localhost'
 port_es = '9200'
 currency = 'EUR'
+
 # Input values
+if (len(sys.argv) >= 3):
+	broker = sys.argv[1]
+	topic = sys.argv[2]
 if (len(sys.argv) >= 5):
-	host_es = sys.argv[1]
-	port_es = sys.argv[2]
+	host_es = sys.argv[3]
+	port_es = sys.argv[4]
+if (len(sys.argv) >= 6):
+	currency = sys.argv[5]
 
 def addElastic(jsonObject):
     date = datetime.strptime(jsonObject["time"]["updated"], "%b %d, %Y %H:%M:%S %Z")
     rate = jsonObject["bpi"][currency]["rate_float"]
     devise = jsonObject["bpi"][currency]["code"]
     es = Elasticsearch([{'host': host_es, 'port': port_es}])
-    es.index(index='cours_btc_idx', doc_type='tempsreel_cours_btc', id=date, body={'date': date, 'rate': rate, 'devise': devise})
+    es.index(index='cours_btc_idx', doc_type='cours_btc', id=date, body={'date': date, 'rate': rate, 'devise': devise, 'data_type': 'temps_reel'})
 
 
 def sendData(tuple):
@@ -51,7 +59,7 @@ def main():
 
     ssc = StreamingContext(sc, 10)
 
-    broker, topic = sys.argv[3:]
+    #broker, topic = sys.argv[3:]
 
     kvs = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": broker})
 
---
+++ cours_bitcoin/tests/test_historique.py
@@ -0,0 +1,16 @@
+'''from python_script import name_function
+
+def test_name_function():
+	sample_input
+	expected_res
+	assert name_function(sample_input) == expected_res'''
+from cours_bitcoin.scripts.simple_add import add
+
+def test_initialize():
+	assert(True)
+	
+def test_add():
+	a, b = 2, 5
+	res = add(a,b)
+	expected_res = 7
+	assert (res == expected_res)
