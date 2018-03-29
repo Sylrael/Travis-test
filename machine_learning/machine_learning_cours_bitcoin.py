dc83e875e715b62a0168fa5a9bdb113b4e16e1db
Sylrael <stephane.tiv@gmail.com>
2018-03-29 14:38:57 +0200

    add machine learning script

--- machine_learning/machine_learning_cours_bitcoin.py
+++ machine_learning/machine_learning_cours_bitcoin.py
@@ -1,22 +1,18 @@
 from pyspark.sql import SparkSession, Row
-from pyspark.ml.feature import CountVectorizer, Word2Vec
+from pyspark.ml.feature import CountVectorizer, StringIndexer
+from pyspark.ml.classification import NaiveBayes
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 import requests
 from elasticsearch import Elasticsearch
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import datetime
 from datetime import timedelta
+import sys
 
 STOPWORDS = set(stopwords.words('english'))
 CHAR_TO_REMOVE = ',.:;?!"()'
-
-#Initialize SparkSession
-spark = SparkSession \
-    .builder \
-    .appName("Machine learning Bitcoin") \
-    .config("master", "local[2]") \
-    .getOrCreate()
-sc = spark.sparkContext
+API_KEYS = 'Your API key'
 
 def convert(input):
     if isinstance(input, dict):
@@ -28,26 +24,37 @@ def convert(input):
     else:
         return input
 		
+def get_next_day(date_str):
+	date = datetime.datetime.strptime(date_str, "%Y-%m-%d")
+	next_day = date + timedelta(days=1)
+	next_date_str = next_day.strftime("%Y-%m-%d")
+	return next_date_str
+		
 def retrieve_bitcoin_cours(date_str):
 	es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 	res = es.get(index='cours_btc_idx_test', doc_type='cours_btc_test', id=date_str)
 	return res
 
 def retrieve_data_day(date_str):
+	date_early = date_str+'T00:00:00'
+	date_late = date_str+'T23:59:59'
 	url_googlenews_api = ('https://newsapi.org/v2/everything?'
        'q=Bitcoin&'
 	   'language=en&'
-       'from='+str(date_str)+'&'
-       'sortBy=popularity&'
+       'from='+date_early+'&'
+	   'to='+date_late+'&'
+       'sortBy=relevance&'
 	   'pageSize=10&'
 	   'page=1&'
-       'apiKey=72c815576bf74fa88660849971ed592e')
+       'apiKey='+API_KEYS)
 	  
 	response = requests.get(url_googlenews_api)
 	if response.status_code == 200:
 		return response.json()
 	else:
-		sys.exit(20)
+		print ('error')
+		return None
+		# sys.exit(20)
 
 #To do: filter number
 def filter_text(text):
@@ -74,13 +81,14 @@ def format_data(raw_data):
 	for i in range (len(raw_data['articles'])):
 		#text_article = (raw_data["articles"][i]["title"])+' '+(raw_data["articles"][i]["description"])
 		text_article = (raw_data["articles"][i]["description"])
+		if text_article == None:
+			return None
 		formatted_words_list = filter_text(text_article)
 		date_article = (raw_data["articles"][i]["publishedAt"])
 		formatted_data.append((date_article, formatted_words_list))
 	return formatted_data
 	
 def extract_features(list_words):
-	Word2Vec
 	return vector
 	
 def prepare_data(rdd):
@@ -90,7 +98,7 @@ def prepare_data(rdd):
 def split_data(rdd):
 	SPLIT_WEIGHT = 0.7
 	(rdd_train, rdd_test) = rdd.randomSplit([SPLIT_WEIGHT, 1.0 - SPLIT_WEIGHT])
-	return (rdd_train, rdd_test)
+	return (spark.createDataFrame(rdd_train), spark.createDataFrame(rdd_test))
 	
 def train_model(rdd_train):
 	
@@ -109,73 +117,74 @@ def get_variation_value(date_str, date_next_str):
 	valeur_day_next = bitcoin_data_date["_source"]["rate"]
 	
 	difference = valeur_day_next - valeur_day
-	# print (valeur_day)
-	# print (valeur_day_next)
-	print (valeur_day_next-valeur_day)
 	return float(1) if difference >= 0 else float(0)
-
-def get_next_day(date_str):
-	date = datetime.datetime.strptime(date_str, "%Y-%m-%d")
-	next_day = date + timedelta(days=1)
-	next_date_str = next_day.strftime("%Y-%m-%d")
-	return next_date_str
 	
 def main():
-	date_start = '2016-01-01'
-	date_end = '2016-01-04'
+	#Initialize SparkSession
+	spark = SparkSession \
+		.builder \
+		.appName("Machine learning Bitcoin") \
+		.config("master", "local[3]") \
+		.getOrCreate()
+	sc = spark.sparkContext
+	
+	date_start = '2018-01-01'
+	date_end = '2018-02-28'
 	date_search = date_start
 	
 	tuples_list = []
+	print (date_search)
 	while (date_search != date_end):
-		data_list = get_aggregated_text(format_data(retrieve_data_day(date_search)))
-		data_list = [mot.encode('utf-8') for mot in data_list]
-		diff = get_variation_value(date_search, get_next_day(date_search))
-		diff_wordslist = (diff, data_list)
-		tuples_list.append(diff_wordslist)
+		data_day = retrieve_data_day(date_search)
+		if data_day != None:
+			formatted_data = format_data(data_day)
+			if formatted_data != None:
+				data_list = get_aggregated_text(formatted_data)
+				#data_list = [mot.encode('utf-8') for mot in data_list]
+				# print data_list
+				diff = get_variation_value(date_search, get_next_day(date_search))
+				diff_wordslist = (diff, data_list)
+				tuples_list.append(diff_wordslist)
 		date_search = get_next_day(date_search)
-		
-	'''data_list = get_aggregated_text(format_data(retrieve_data_day(date_search)))
-	#data_list = [mot.encode('utf-8') for mot in data_list]
-	for i in data_list:
-		print i.encode('utf-8')'''
-
-	'''date = '2017-01-05'
-	date_suiv = get_next_day(date)
-	date_next2 = get_next_day(date_suiv)
-	diff_5_6 = get_variation_value(date, date_suiv)
-	diff_6_7 = get_variation_value(date_suiv, date_next2)
-	# Retrieve data from GoogleNews API
-	raw_data = retrieve_data_day(date)
-	# Format data from raw data to list of tuple (date, article_description) and filter ponctuations, stopwords and empty words
-	formatted_data = format_data(raw_data)
-	# Aggregate data from list of tuple to a single list of words
-	aggregated_data_j1 = get_aggregated_text(formatted_data)
-	aggregated_data_j1 = [mot.encode('utf-8') for mot in aggregated_data_j1]
-	tuple1 = (diff_5_6, aggregated_data_j1)
 	
-	aggregated_data_j2 = get_aggregated_text(format_data(retrieve_data_day(date_suiv)))
-	aggregated_data_j2 = [mot.encode('utf-8') for mot in aggregated_data_j2]
-	tuple2 = (diff_6_7, aggregated_data_j2)
-	
-	tuples_list = []
-	tuples_list.append(tuple1)
-	tuples_list.append(tuple2)'''
-	
+	# print tuples_list
+	#rdd dataframe
 	rdd = sc.parallelize(tuples_list)
 	rdd = rdd.map(lambda (indice, list): Row(diff=indice, words=list))
+	df_train, df_test = split_data(rdd)
+	#df_data = spark.createDataFrame(rdd)
 	
-	df_data = spark.createDataFrame(rdd)
-	
+	# Vectorizer : extract features from list of words
+	# Estimator type CountVectorizer
 	count_vectorizer = CountVectorizer(inputCol='words', outputCol='features')
+	vectorizer_transformer = count_vectorizer.fit(df_train)
 	
-	model_cv = count_vectorizer.fit(df_data)
+	# Apply Transformer
+	train_bag_of_words = vectorizer_transformer.transform(df_train)
+	test_bag_of_words = vectorizer_transformer.transform(df_test)
 	
-	result = model_cv.transform(df_data)
-	print('aaaaaaaaaaaaaa')
-	print result.collect()
-	#rdd = rdd.map(lambda (indice, list): (indice, list[0]))
-	#print (aggregated_data)
-
+	# Indexer : extract label from difference in value from two days
+	# Indexer for indexing difference value, up or down (constant ?)
+	label_indexer = StringIndexer(inputCol='diff', outputCol='label_index')
+	label_indexer_transformer = label_indexer.fit(train_bag_of_words)
+	
+	# Apply transformer
+	train_bag_of_words = label_indexer_transformer.transform(train_bag_of_words)
+	test_bag_of_words = label_indexer_transformer.transform(test_bag_of_words)
+	
+	# Classifier type : NaiveBayes
+	# Train
+	classifier = NaiveBayes(labelCol='label_index', featuresCol='features', predictionCol='label_predicted')
+	classifier_transformer = classifier.fit(train_bag_of_words)
+	# Apply classifier trained on test data
+	# Test
+	test_predicted = classifier_transformer.transform(test_bag_of_words)
+	# Evaluator
+	# Evaluation
+	evaluator = MulticlassClassificationEvaluator(labelCol='label_index', predictionCol='label_predicted', metricName='accuracy')
+	accuracy = evaluator.evaluate(test_predicted)
+	
+	print ('Accuracy : '+ str(accuracy*100)+'%')
 
 if __name__ == "__main__":
 	main()
