9c2efa5e76b69c46ab8f54d140b6091adbf0e566
Stéphane <stephane.tiv@gmail.com>
2018-04-03 18:21:57 +0200

    update lstm neural network algo and add saved model for prediction

--- machine_learning/machine_learning_neural_network.py
+++ machine_learning/machine_learning_neural_network.py
@@ -4,11 +4,13 @@ from datetime import date
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import LSTM, Dense
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 import math
+
+model_path = 'C:/Users/diginamic/Desktop/Travis-test/machine_learning/model'
 
 # Retrieve data from CoinDesk API
 def get_historique_data(currency, start_date, end_date):	
@@ -25,11 +27,14 @@ def create_dataset(dataset):
     dataX.append(dataset[i])
     dataY.append(dataset[i + 1])
   return np.asarray(dataX), np.asarray(dataY)
-		
+
+def save_model(model, model_path):
+	model.save(model_path)
+  
 def main():
-	data_historique = get_historique_data('EUR', '2017-01-01', '2018-01-01')
+	data_historique = get_historique_data('EUR', '2011-01-01', '2018-04-02')
 	data = data_historique.json()
-	dataset = []
+	dataset_raw = []
 	date = []
 	rate = []
 	for i in data["bpi"]:
@@ -37,12 +42,12 @@ def main():
 		value_event = data["bpi"][i]
 		date.append(date_event)
 		rate.append(value_event)
-		dataset.append(value_event)
+		dataset_raw.append(value_event)
 		
 	# plt.plot(date, rate)
 	# plt.show()
 	
-	dataset = np.asarray(dataset).reshape(-1,1)
+	dataset = np.asarray(dataset_raw).reshape(-1,1)
 	scaler = MinMaxScaler(feature_range=(0,1))
 	dataset = scaler.fit_transform(dataset)
 
@@ -50,35 +55,48 @@ def main():
 	dataX, dataY = create_dataset(dataset)
 	# Split data into training data and testing data 80/20
 	trainX, testX, trainY, testY = train_test_split(dataX, dataY, test_size=0.20, shuffle=False)
-	# print (trainX)
+	# Reshape to fit input requirement for LSTM neural network
 	trainX = np.reshape(trainX, (trainX.shape[0], 1, 1))
 	testX = np.reshape(testX, (testX.shape[0], 1, 1))
 	
 	# Create LSTM Network
-	model = Sequential()
+	'''model = Sequential()
 	model.add(LSTM(4, input_shape=(1,1)))
 	model.add(Dense(1))
 	model.compile(loss='mean_squared_error', optimizer='adam')
-	model.fit(trainX, trainY, epochs=5, batch_size=1, verbose=2)
+	model.fit(trainX, trainY, epochs=5, batch_size=1, verbose=2)'''
 	
-	#
-	# copy/paste
-	#
-	# make predictions
+	# Save model
+	# save_model(model, model_path)
+	# Load model
+	model = load_model(model_path)
+	
+	# Make predictions
 	trainPredict = model.predict(trainX)
 	testPredict = model.predict(testX)
-	# invert predictions
+	
+	futurePredict = model.predict(np.asarray([[testPredict[-1]]]))
+	futurePredict = scaler.inverse_transform(futurePredict)
+	
+	# Invert predictions to get BTC value back 
 	trainPredict = scaler.inverse_transform(trainPredict)
 	trainY = scaler.inverse_transform(trainY)
 	testPredict = scaler.inverse_transform(testPredict)
 	testY = scaler.inverse_transform(testY)
-	# calculate root mean squared error
+	
+	print ('Value for last 5 days')
+	print (dataset_raw[-5:])
+	print ('predicted Value for last 5 days')
+	print (testPredict[-5:])
+	print ('Bitcoin value for tomorrow : ', futurePredict)
+	
+	# Calculate root mean squared error
 	trainScore = math.sqrt(mean_squared_error(trainY[:,0], trainPredict[:,0]))
 	print('Train Score: %.2f RMSE' % (trainScore))
 	testScore = math.sqrt(mean_squared_error(testY[:,0], testPredict[:,0]))
 	print('Test Score: %.2f RMSE' % (testScore))
 	
-	# shift train predictions for plotting
+	'''# shift train predictions for plotting
 	trainPredictPlot = np.empty_like(dataset)
 	trainPredictPlot[:, :] = np.nan
 	trainPredictPlot[1:len(trainPredict)+1, :] = trainPredict
@@ -86,11 +104,12 @@ def main():
 	testPredictPlot = np.empty_like(dataset)
 	testPredictPlot[:, :] = np.nan
 	testPredictPlot[len(trainPredict):len(dataset)-1, :] = testPredict
+	
 	# plot baseline and predictions
 	plt.plot(scaler.inverse_transform(dataset))
 	plt.plot(trainPredictPlot)
 	plt.plot(testPredictPlot)
-	plt.show()
+	plt.show()'''
 	
 if __name__ == '__main__':
 	main()
